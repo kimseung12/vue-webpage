41aacd9b20fe3fb96f06e0770b37ccf3a9794960
kimseung12 <tmddn113@gmail.com>
2023-10-30 13:14:02 +0900

    2023-10-30

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Header from "@/components/Header.vue"
+import Footer from "@/components/Footer.vue"
+import Silder from "@/components/Silder.vue"
+import Image from "@/components/Image.vue"
+import ImgText from "@/components/ImgText.vue"
+import Card from "@/components/Card.vue"
+import Banner from "@/components/Banner.vue"
+import Text from "@/components/Text.vue"
+</script>
+
+<template>
+  <Header />
+  <main>
+    <Silder />
+    <Image />
+    <ImgText />
+    <Card />
+    <Banner />
+    <Text />
+  </main>
+  <Footer />
+</template>
