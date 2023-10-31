b29f3f09ec3ca49bdc5643f13e8e0bc3811d4fe9
kimseung12 <tmddn113@gmail.com>
2023-10-31 11:05:03 +0900

    2023-10-31

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,14 +10,14 @@ import Text from "@/components/Text.vue"
 </script>
 
 <template>
-  <Header />
+  <Header element="nexon" />
   <main>
-    <Silder />
-    <Image />
-    <ImgText />
-    <Card />
-    <Banner />
-    <Text />
+    <Silder element="nexon" />
+    <Image element="section nexon" title="포트폴리오가 실력이다." />
+    <ImgText element="section nexon gray" title="이미지 텍스트 유형" />
+    <Card element="section nexon" title="웹스토리보이 강의" />
+    <Banner element="nexon" title="배너 영역" />
+    <Text element="section nexon" title="텍스트 유형01" />
   </main>
-  <Footer />
+  <Footer element="nexon section gray" />
 </template>
